e66ba16758e1991c2aa5305531bd3297690c4063
Lisa Garvey <garveygeneral@gmail.com>
2020-02-28 10:08:18 -0800

    update content with calculations

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  var program = $("input:radio[name=program]:checked").val();
-}
+  var answer1 = parseInt($("input:radio[name=answer1]:checked").val());
+  var answer2 = parseInt($("input:radio[name=answer2]:checked").val());
+  var answer3 = parseInt($("input:radio[name=answer3]:checked").val());
+ 
+  if (answer1 === 5 || answer1 < 5) {
+    $('#answers').show();
+  } else if (answer2 > 5 && answer2 < 10 || answer2 === 10) {
+    $('#answers').show();
+  } else if (answer3 > 10 && answer3 < 15 || answer3 === 15) {
+    $('#answers').show();
+  };
+  $("#output").text(result);
+
+  $(".clickable").click(function() {
+    $(".answer-showing").show();
+  });
+}); 
